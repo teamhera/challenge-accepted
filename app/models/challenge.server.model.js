5d994000b5c2887a64341e47e7192532089315d3
yakomaru <parrish.nate@gmail.com>
2015-08-06 21:22:10 -0700

    added task and challenge schema

---
+++ app/models/challenge.server.model.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var ChallengeSchema = new Schema({
+  name: {
+    type: String,
+    default: '',
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  reward: {
+    type: String,
+    default: ''
+  },
+  tasks: {
+    type: Array,
+    default: []
+  }
+});
