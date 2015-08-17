b1513f3d6e2b23a05e74b3c049aeba4ebb7acd5c
Charles Crame <ccrame@gmail.com>
2015-08-17 00:10:58 -0700

    Added redirect via  if user is already logged in

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,16 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
-	}
+angular.module('core').controller('HomeController', ['$scope', '$location','Authentication',
+  function($scope, $location, Authentication) {
+    // This provides Authentication context.
+    $scope.authentication = Authentication;
+
+    //this function ensures redirect to /user-to-do after login
+    $scope.$watch(function(){return $location.path();}, function(next,current){
+      if(Authentication.user && current === '/'){
+        $location.path('/user-to-do');
+      }
+    });
+  }
 ]);
